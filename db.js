d5faeb29bd15f39a487d8dcd5a75f3a9d481f7c9
evil <evil@evil.evil>
2024-01-29 19:46:59 +0500

    initial

---
+++ db.js
@@ -0,0 +1,57 @@
+const products = [
+  {
+    id: 1,
+    name: "Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»",
+    image: "./assets/moloko.png",
+    rate: 4.5,
+    price_with_card: "69.99 ₽",
+    price_without_card: "140,50 ₽",
+    country: "Russia",
+    massa: "250ml",
+  },
+  {
+    id: 2,
+    name: "Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»",
+    image: "./assets/colbasa.png",
+    rate: 3.8,
+    price_with_card: "599,99 ₽",
+    price_without_card: 24.99,
+    country: "Russia",
+    massa: "200g",
+  },
+  {
+    id: 3,
+    name: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    image: "./assets/colbasa-m.png",
+    rate: 4.2,
+    price_with_card: "44,50 ₽",
+    price_without_card: 59.99,
+    country: "United Kingdom",
+    massa: "300g",
+  },
+
+  {
+    id: 3,
+    name: "Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»",
+    image: "./assets/sosiska.png",
+    rate: 4.2,
+    price_with_card: "159,99 ₽",
+    price_without_card: 59.99,
+    country: "Russia",
+    massa: "300g",
+  },
+  {
+    id: 3,
+    name: "Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»",
+    image: "./assets/yogurt.png",
+    rate: 4.2,
+    price_with_card: "29,99 ₽",
+    price_without_card: 59.99,
+    country: "Russia",
+    massa: "300g",
+  },
+];
+
+
+
+export default products;
